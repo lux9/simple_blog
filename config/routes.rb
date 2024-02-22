b5a7456126ba87f7049f1f93f88d9e8fa010da83
not lux <luischn94@gmail.com>
2024-02-22 17:52:51 -0300

    sets home path to  posts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
+  root "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Users routes
