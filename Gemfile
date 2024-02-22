49e0c9e0f906fe9d1630141c1470c09e2667865a
not lux <luischn94@gmail.com>
2024-02-22 17:01:37 -0300

    seeds database with fake records

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby "3.1.2"
 # Users setup
 gem "devise"
 
+# Fake data generator
+gem "faker"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,51 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+require_relative "seed_helper.rb"
+
+seed_start_time = Time.now
+puts "Starting seeds"
+if Rails.env == "development"
+
+  puts "Destroying old records..."
+  destroy_old_records
+
+  puts "Creating new users..."
+  10.times do
+    new_user = User.new(
+      email: Faker::Artist.unique.name.downcase.gsub(" ", "_") + "@mail.com",
+      password: "simpleblog"
+    )
+    new_user.save!
+    puts "user #{new_user.email} created"
+
+    puts "Creating posts for #{new_user.email}..."
+    5.times do
+      new_post = new_user.posts.build(
+        title: Faker::Restaurant.unique.name,
+        body: Faker::Restaurant.description
+      )
+      new_post.save!
+      puts "post #{new_post.id} created"
+    end
+  end
+end
+
+puts "Creating comments for users..."
+User.all.each do |user|
+  commentable_posts = Post.where.not(user: user)
+  5.times do
+    new_comment = user.comments.build(
+      content: Faker::Restaurant.review,
+      post: commentable_posts.sample,
+      user: user
+    )
+    new_comment.save!
+    puts "comment #{new_comment.id} created for post #{new_comment.post.id}"
+  end
+end
+
+puts '-------------------'
+puts 'Ending seeds'
+seed_end_time = Time.now
+puts "Seeding took #{(seed_end_time - seed_start_time).round(3)} secs"
+puts '-------------------'
+display_records_amount
