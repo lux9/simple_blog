dd248f25b1baa85a009347a9bed68b7d37ca1944
not lux <luischn94@gmail.com>
2024-02-23 00:52:34 -0300

    authenticates user rol at admin dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :check_admin, only: [:admin]
+    
   def profile
     @user = User.find(params[:user_id])
     @posts = @user.posts
@@ -6,5 +8,11 @@ class UsersController < ApplicationController
 
   def admin
     @posts = Post.all.order(created_at: :desc)
+  end
+
+  private
+
+  def check_admin
+    redirect_to(root_url) unless current_user && current_user.admin?
   end
 end
