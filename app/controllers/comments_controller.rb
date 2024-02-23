47975cc1905c3e5d72422ce6a698cc58a8c4bf4c
not lux <luischn94@gmail.com>
2024-02-22 21:37:21 -0300

    builds user clear_username method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,15 +8,22 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.post = Post.find(params[:post_id])
-    @comment.user = User.first
+    @comment.user = current_user
+
     if @comment.save
-      redirect_to @comment.post, notice: 'Comment was successfully created.'
+      respond_to do |format|
+        format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
+        format.text { render partial: "comments/comment", locals: { comment: @comment }, formats: [:html] }
+        # format.text { render partial: "comments/list", locals: { post: @comment.post }, formats: [:html] }
+      end
     else
-      @post = @comment.post
-      render 'posts/show', status: :unprocessable_entity
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.text { render plain: "Error saving comment", status: :unprocessable_entity }
+      end
     end
   end
-
+  
   def edit
   end
 
